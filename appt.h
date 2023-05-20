853e0ca772947ce7c1877a5d548469465d6fef14
bugger <usernamesarenoteasy@proton.me>
2023-05-19 21:36:20 -0600

    made it so you can't just make the cursor disappear by going offscreen

--- appt.h
+++ appt.h
@@ -1,7 +1,10 @@
+#include <ncurses.h>
+
 #ifndef APPT_H
 #define APPT_H
 
-void promptAppointment(WINDOW** apptWin, char* monthName, int monthDay, int year, int monthInt);
-void printAppointments(WINDOW** apptWin);
+void promptAppointment(WINDOW **apptWin, char *monthName, int monthDay,
+                       int year, int monthInt);
+void printAppointments(WINDOW **apptWin);
 
 #endif
--- utils.h
+++ utils.h
@@ -1,9 +1,12 @@
+#include <ncurses.h>
+
 #ifndef UTILS_H
 #define UTILS_H
 
-char* setMonthInfo(int year, int month, int* monthDays);
+char *setMonthInfo(int year, int month, int *monthDays);
 void sort(int array[], int size, int dateArray[]);
-void moveForSunday(int daysToMove, int* cursorX);
-void refreshCal(int daysToMove, int* cursorX, int* cursorY, int* highlight, WINDOW** win, int multiplier);
+void moveForSunday(int daysToMove, int *cursorX);
+void refreshCal(int daysToMove, int *cursorX, int *cursorY, int *highlight,
+                WINDOW **win, int multiplier);
 
 #endif
